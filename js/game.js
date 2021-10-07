3adea74f351a54f08fc91049fa21b6e97f448134
Nelson Barrios <Nelson.barrios3@gmail.com>
2021-10-07 12:18:49 -0300

    update count-card

--- js/game.js
+++ js/game.js
@@ -126,16 +126,16 @@ function init(){
 				titleText.center(1, 1);
 				var nameInput = new TextInput();
 				// autofocus
-				nameInput._focused = true;
-				nameInput._hiddenInput.style.display = 'block';
-				nameInput._hiddenInput.style.left = (nameInput.x + stage.canvas.offsetLeft + nameInput._padding) + 'px';
-				nameInput._hiddenInput.style.top = (nameInput.y + stage.canvas.offsetTop + nameInput._padding) + 'px';
-				nameInput._hiddenInput.focus();
+				//nameInput._focused = true;
+				//nameInput._hiddenInput.style.display = 'block';
+				//nameInput._hiddenInput.style.left = (nameInput.x + stage.canvas.offsetLeft + nameInput._padding) + 'px';
+				//nameInput._hiddenInput.style.top = (nameInput.y + stage.canvas.offsetTop + nameInput._padding) + 'px';
+				//nameInput._hiddenInput.focus();
 				nameInput.x = 430;
-				nameInput.y = 400;
+				//nameInput.y = 400;
 				nameInput._visiblePostCursorText.text = 'Tu nombre';
 
-				var submitText = new createjs.Text('OK', '30px Arial', '#fff');
+				var submitText = new createjs.Text('Iniciar', '30px Arial', '#fff');
 				submitText.x = 640;
 				submitText.y = 403;
 				submitText.cursor = 'Pointer';
