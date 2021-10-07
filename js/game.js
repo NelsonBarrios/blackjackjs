601d0f267f312fcbe6cb92e752e44ad575bb6fd9
Nelson Barrios <Nelson.barrios3@gmail.com>
2021-10-07 13:12:00 -0300

    update count-card

--- js/game.js
+++ js/game.js
@@ -243,7 +243,7 @@ function init(){
 			});
 			
 				setTimeout(function () {
-					this.addCount(total)},1000);
+					this.addCount(total)},3000);
 
 			return total; //mostrar total contado
 		},
