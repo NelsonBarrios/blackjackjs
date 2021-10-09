c26aa1d44f552d94820d11dcd62806ed5dbed985
Nelson Barrios <Nelson.barrios3@gmail.com>
2021-10-09 01:03:45 -0300

    update count-card

--- js/game.js
+++ js/game.js
@@ -253,7 +253,7 @@ function init(){
 		},
 
 		distributeCard: function(to, hidden = false){ //distribute of cards and deleting it
-			game.addCount(0);
+			game.addCount(0).removeAllChildren();
 			var index = rand(0, this.deck.length - 1);
 			var card = this.deck[index];
 			if(hidden) card.hidden = true;
